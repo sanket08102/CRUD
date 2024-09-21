15f5ae2228e99119b04d517d670c5fac5baf06c5
sanket08102 <88883718+sanket08102@users.noreply.github.com>
2024-09-21 20:16:45 +0530

    Update Add.jsx

--- client/src/components/adduser/Add.jsx
+++ client/src/components/adduser/Add.jsx
@@ -21,7 +21,7 @@ const Add = () => {
 
     const submitForm = async(e) =>{
         e.preventDefault();
-        await axios.post("http://localhost:8000/api/create", user)
+        await axios.post("https://crud-app-3iae.onrender.com/api/create", user)
         .then((response)=>{
             toast.success(response.data.msg, {position:"top-right"})
             navigate("/")
