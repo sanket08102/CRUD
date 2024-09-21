66fa35614459d43135c1cafbcbb7db162e72351b
sanket08102 <88883718+sanket08102@users.noreply.github.com>
2024-09-21 23:17:57 +0530

    Update Add.jsx

--- client/src/components/adduser/Add.jsx
+++ client/src/components/adduser/Add.jsx
@@ -4,90 +4,89 @@ import axios from "axios";
 import toast from 'react-hot-toast';
 
 const Add = () => {
-
     const users = {
-        fname:"",
-        lname:"",
-        email:"",
-        password:""
-    }
-    const[user, setUser] = useState(users);
+        fname: "",
+        lname: "",
+        email: "",
+        password: ""
+    };
+    const [user, setUser] = useState(users);
     const navigate = useNavigate();
 
-    const inputHandler = (e) =>{
-        const {name, value} = e.target;
-        setUser({...user, [name]:value});
-    }
+    const inputHandler = (e) => {
+        const { name, value } = e.target;
+        setUser({ ...user, [name]: value });
+    };
 
-    const submitForm = async(e) =>{
+    const submitForm = async (e) => {
         e.preventDefault();
         await axios.post("https://crud-app-server-gjdw.onrender.com/api/create", user)
-        .then((response)=>{
-            toast.success(response.data.msg, {position:"top-right"})
-            navigate("/")
-        })
-        .catch(error => console.log(error))
-    }
+            .then((response) => {
+                toast.success(response.data.msg, { position: "top-right" });
+                navigate("/");
+            })
+            .catch(error => console.log(error));
+    };
 
-  return (
-    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
-      <div className="w-50 bg-white rounded p-3">
-        <Link to={"/"} className='btn btn-success'>Back</Link>
-        <form onSubmit={submitForm}>
-          <h2>Add User</h2>
-          <div className="mb-2">
-            <label htmlFor="fname">First Name</label>
-            <input
-              type="text"
-              name="fname"
-              id="fname"
-              autoComplete='off'
-              placeholder="Firt Name"
-              className="form-control"
-              onChange={inputHandler}
-            />
-          </div>
-          <div className="mb-2">
-            <label htmlFor="lname">Last Name</label>
-            <input
-              type="text"
-              name="lname"
-              id="lname"
-              autoComplete='off'
-              placeholder="Last Name"
-              className="form-control"
-              onChange={inputHandler}
-            />
-          </div>
-          <div className="mb-2">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              autoComplete='off'
-              placeholder="Enter Email"
-              className="form-control"
-              onChange={inputHandler}
-            />
-          </div>
-          <div className="mb-2">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              autoComplete='off'
-              placeholder="Password"
-              className="form-control"
-              onChange={inputHandler}
-            />
-          </div>
-          <button type='submit' className="btn btn-success">Submit</button>
-        </form>
-      </div>
-    </div>
-  )
+    return (
+        <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+            <div className="w-100 w-md-50 bg-white rounded p-3">
+                <Link to={"/"} className='btn btn-success mb-3'>Back</Link>
+                <form onSubmit={submitForm}>
+                    <h2>Add User</h2>
+                    <div className="mb-2">
+                        <label htmlFor="fname">First Name</label>
+                        <input
+                            type="text"
+                            name="fname"
+                            id="fname"
+                            autoComplete='off'
+                            placeholder="First Name"
+                            className="form-control"
+                            onChange={inputHandler}
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="lname">Last Name</label>
+                        <input
+                            type="text"
+                            name="lname"
+                            id="lname"
+                            autoComplete='off'
+                            placeholder="Last Name"
+                            className="form-control"
+                            onChange={inputHandler}
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            autoComplete='off'
+                            placeholder="Enter Email"
+                            className="form-control"
+                            onChange={inputHandler}
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            autoComplete='off'
+                            placeholder="Password"
+                            className="form-control"
+                            onChange={inputHandler}
+                        />
+                    </div>
+                    <button type='submit' className="btn btn-success w-100">Submit</button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
-export default Add
+export default Add;
