b69eb86bdf3a7345641e738b3637d303c8ffb5ea
sanket08102 <88883718+sanket08102@users.noreply.github.com>
2024-09-21 23:44:41 +0530

    Update User.jsx

--- client/src/components/getuser/User.jsx
+++ client/src/components/getuser/User.jsx
@@ -28,7 +28,7 @@ const User = () => {
   };
 
   return (
-    <div className="d-flex vh-100 w-100 bg-primary justify-content-center align-items-center">
+    <div className="d-flex vh-100 w-100 bg-cover bg-primary justify-content-center align-items-center">
       <div className='w-50 bg-white rounded p-3'>
         <Link to="/add" className='btn btn-success mb-3'>Add +</Link>
         <table className='table table-responsive'>
